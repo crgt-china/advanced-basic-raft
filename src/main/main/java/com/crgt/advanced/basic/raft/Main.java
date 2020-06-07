99cddb4d82c1a387e882c2c5e1269ba469f22b75
tao.sun <123456789>
2020-06-07 16:46:54 +0800

    新增角色图例

---
+++ src/main/main/java/com/crgt/advanced/basic/raft/Main.java
@@ -0,0 +1,22 @@
+package com.crgt.advanced.basic.raft;
+
+import java.sql.SQLOutput;
+
+/**
+ * @ClassName Main
+ * @Description TODO
+ * @Author justin.sun@crgecent.com
+ * @Date 2020/6/6 15:47
+ * @Version 1.0
+ **/
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello,raft");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i=" + i);
+        }
+    }
+
+}
